5e9fc5efaa6e97de8d4cff2564bb06fdd420e8b0
Davi Maurício <davimauricio21@gmail.com>
2023-04-12 21:47:59 -0300

    adicionando html

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as login_django
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+def cadastro(request):
+    if request.method == "GET":
+        return render(request, 'cadastro.html')
+    else:
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = User.objects.filter(username=username).first()
+
+        if user:
+            return HttpResponse('já existe um usuário com esse nome!')
+        
+        user = User.objects.create_user(username=username, email=email, 
+                                        password=senha)
+        user.save()
+
+        return HttpResponse('Usuário cadastrado com sucesso')
+    
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            login_django(request, user)
+
+            return HttpResponse('autenticado')
+        else:
+            return HttpResponse('email ou senha invalidos')
+
+
+@login_required(login_url="/auth/login/")
+def plataforma(request):
+    return HttpResponse('plataforma')
