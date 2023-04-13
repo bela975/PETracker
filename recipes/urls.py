d065212a41510d8e169fe67ae7964f929778ffbe
bela975 <113048987+bela975@users.noreply.github.com>
2023-04-13 03:39:59 -0300

    adding calendar html, urls, views and models

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
+from django.conf.urls import url
 from . import views
 
+appName = 'PETracker'
 urlpatterns = [
     path('', views.login, name='login'),
+    url(r'^index/$', views.index, name='index'),
+    url(r'^calendar/$', views.CalendarView.as_view(), name='calendar'),
 ]
+
--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,12 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib.auth import login as login_django
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
+from django.views import generic
+from django.utils.safestring import mark_safe
 
+from .models import *
+from .utils import Calendar
 
 def cadastro(request):
     if request.method == "GET":
@@ -46,3 +51,30 @@ def login(request):
 @login_required(login_url="/auth/login/")
 def plataforma(request):
     return HttpResponse('plataforma')
+
+def index(request):
+    return HttpResponse('hello')
+
+class CalendarView(generic.ListView):
+    model = Event
+    template_name = 'PETracker/calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # use today's date for the calendar
+        d = get_date(self.request.GET.get('day', None))
+
+        # Instantiate our calendar class with today's year and date
+        cal = Calendar(d.year, d.month)
+
+        # Call the formatmonth method, which returns our calendar as a table
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        return context
+
+def get_date(req_day,date):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
