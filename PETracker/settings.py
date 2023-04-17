a6cedf316bd9b21418d36efff72e46331876bd77
bela975 <113048987+bela975@users.noreply.github.com>
2023-04-16 22:01:08 -0300

    fixing settings for aws deploy

--- PETracker/settings.py
+++ PETracker/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'PETracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'utils.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 
