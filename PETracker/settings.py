3a92e0792e6275148b9df392b3be32e5c360260b
Davi Maurício <davimauricio21@gmail.com>
2023-05-15 18:01:19 -0300

    erro 502
    arrumando erro 502 banco de dados

--- PETracker/settings.py
+++ PETracker/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'PETracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'utils.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
