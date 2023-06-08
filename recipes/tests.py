b5c246e22b635979d83c367c8b72eecd7e5f7348
Virna Amaral <vfpa@cesar.school>
2023-06-07 21:54:43 -0300

    teste alterações arquivo django.yml 8

--- recipes/tests.py
+++ recipes/tests.py
@@ -236,7 +236,7 @@ class petracker_tests(LiveServerTestCase):
         medicine_diary_add_button.click()
         time.sleep(1)
         # med_info = driver.find_element(By.ID, "med-det")
-        med_info = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, ".list-group-item.list-group-item-action#med-det")))
+        med_info = WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.ID, "med-det")))
         med_info.click()
         # driver.execute_script("arguments[0].click();", med_info)
 
