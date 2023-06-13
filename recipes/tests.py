08921e4835161390af556e819ed286f8e37fca6a
Virna Amaral <vfpa@cesar.school>
2023-06-12 23:01:05 -0300

    alterações finais testes

--- recipes/tests.py
+++ recipes/tests.py
@@ -23,7 +23,7 @@ class petracker_tests(LiveServerTestCase):
         self.run_tests(driver)
     
     def register(self, driver):
-        time.sleep(1)
+        time.sleep(10)
         register = driver.find_element(By.NAME, "sign-up")
         driver.execute_script("arguments[0].click();", register)
         register_username = driver.find_element(By.NAME, "username")
@@ -35,6 +35,7 @@ class petracker_tests(LiveServerTestCase):
         register_button = driver.find_element(By.NAME, "register")
         time.sleep(1)
         driver.execute_script("arguments[0].click();", register_button)
+        print("REGISTER TEST OK")
 
     def login(self, driver):
         driver.get("http://ptv123-env.eba-gb4zztkd.sa-east-1.elasticbeanstalk.com/")
@@ -46,6 +47,7 @@ class petracker_tests(LiveServerTestCase):
         login_button = driver.find_element(By.NAME, "login")
         time.sleep(1)
         login_button.click()
+        print("LOGIN TEST OK")
 
     def register_pet(self, driver):
         time.sleep(1)
@@ -63,16 +65,20 @@ class petracker_tests(LiveServerTestCase):
         register_pet_phone.send_keys("12345678910")
         register_pet_email = driver.find_element(By.NAME, "email")
         register_pet_email.send_keys("spiderman@gmail.com")
+        # driver.find_element(By.NAME, "photo").send_keys("C:/Users/virna/OneDrive/Área de Trabalho/PETracker/files/spider_pig.jpg") # utilizado para teste local com link aws
         register_pet_button_create = driver.find_element(By.NAME, "create")
         time.sleep(2)
-        
         register_pet_button_create.click()
+        print("REGISTER PET TEST OK")
         
+
     def acessing_home(self, driver):
         time.sleep(1)
         pet_profile_button = driver.find_element(By.ID, "pet_profile")
         driver.execute_script("arguments[0].click();", pet_profile_button)
         time.sleep(1)
+        print("ACESSING HOME PET TEST OK")
+
 
     def alergy(self, driver):
         alergy_input = driver.find_element(By.NAME, "title")
@@ -84,7 +90,8 @@ class petracker_tests(LiveServerTestCase):
         time.sleep(1)
         driver.find_element(By.ID, "del-al").click()
         time.sleep(1)
-        
+        print("ALERGY TEST OK")
+
     def background_color(self, driver):
         background_color_select = driver.find_element(By.ID, "id_background_color")
         background_color_select.click()
@@ -113,6 +120,8 @@ class petracker_tests(LiveServerTestCase):
         background_color_select_button = driver.find_element(By.NAME, "add_color")
         background_color_select_button.click()
         time.sleep(2)
+        print("BACKGROUND COLOR TEST OK")
+
 
     def calendar(self, driver):
         calendar_nav_button = driver.find_element(By.ID, "calendar")
@@ -143,6 +152,8 @@ class petracker_tests(LiveServerTestCase):
         back_button = driver.find_element(By.ID, "back_button")
         back_button.send_keys(Keys.RETURN)
         time.sleep(1)
+        print("CALENDAR TEST OK")
+
 
     def checklist(self, driver):
         checklist_nav_button = driver.find_element(By.ID, "kanban") 
@@ -191,6 +202,8 @@ class petracker_tests(LiveServerTestCase):
         back_button = driver.find_element(By.ID, "back_button")
         back_button.click()
         time.sleep(2)
+        print("CHECKLIST TEST OK")
+
 
     def medicine(self, driver):
         medicine_nav_button = driver.find_element(By.ID, "medicine")
@@ -258,6 +271,8 @@ class petracker_tests(LiveServerTestCase):
         back_button = driver.find_element(By.ID, "back_button")
         back_button.click()
         time.sleep(2)
+        print("MEDICINE TEST OK")
+
         
     def food(self, driver):
         time.sleep(2)
@@ -282,11 +297,14 @@ class petracker_tests(LiveServerTestCase):
         back_button = driver.find_element(By.ID, "back_button")
         back_button.click()
         time.sleep(2)
+        print("FOOD TEST OK")
+
     
     def logout(self, driver):
         logout_button = driver.find_element(By.ID, "Logout")
         logout_button.click()
         time.sleep(2)
+        print("LOGOUT TEST OK")
 
         driver.quit()
 
@@ -304,3 +322,5 @@ class petracker_tests(LiveServerTestCase):
         self.medicine(driver)
         self.food(driver)
         self.logout(driver)
+        print("ALL TESTS OK")
+
